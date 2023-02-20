b1bac17861592f7899b44e2160dc42424c030e15
joker69216 <hamaraad883@gmail.com>
2023-02-21 00:59:33 +0300

    add redux

--- app/javascript/components/Greets.jsx
+++ app/javascript/components/Greets.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingsSlice';
 
 const Greeting = () => {
+  const greetingShow = useSelector((state) => state.greetings.message);
+  const status = useSelector((state) => state.greetings.status);
+  const dispatch = useDispatch();
+  let content;
+  if (status === 'succeeded') {
+    content = greetingShow.message;
+  }
 
   return (
     <>
-      <h1>Hello, world!</h1>
+      <h1>Random Greeting:</h1>
+      <p>{content}</p>
+      <button type='button' onClick={() => dispatch(fetchGreeting())}>
+        generate new
+      </button>
     </>
   );
 };
